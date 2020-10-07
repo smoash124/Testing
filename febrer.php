9d99e56f61ef71709f930eb9db51e7b72fde2a43
carlos <carloscardonavizcarro@gmail.com>
2020-10-07 09:35:23 +0200

    New line febrer

--- febrer.php
+++ febrer.php
@@ -1,1 +1,2 @@
 echo'Febrer,Febreret,set capes i un barret';
+echo'Febrer,Febreret,set capes i un barret';
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 echo'Al gener,tancala porta i encen el braser';
 echo'Aigua de gener tot l'any va be';
 echo'Les gelades de gener fan florir l'ametller':
+echo'Aigua de gener tot l'any va be';
